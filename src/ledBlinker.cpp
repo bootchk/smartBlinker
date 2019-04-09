0f53a9ab1f58892fa47205b0fe28b12dda03224f
bootchk <bootch@nc.rr.com>
2019-04-09 18:38:00 -0400

    accelerated blink weaker/shorter

--- src/ledBlinker.cpp
+++ src/ledBlinker.cpp
@@ -6,6 +6,8 @@
 #include <LEDAndLightSensor/ledAndLightSensor.h>
 #include <timer/timer.h>
 
+#include "parameters.h"
+
 
 
 
@@ -20,9 +22,17 @@ void LEDBlinker::blink()
     // Same LED as used to measure light, now used to generate light
     LEDAndLightSensor::toOnFromOff();
 
+
+
+#ifdef ACCELERATED_TIME_PARAMETERS
+    // 2 millisecond is easily visible if staring from close distance
+    LowPowerTimer::delayTwoMilliSeconds();
+#else
     // Budget calcs used 12 mS
+    // Designed to be visible for hundreds of meters even if not staring
     LowPowerTimer::delayTenMilliSeconds();
     //LowPowerTimer::delayTwentyMilliSeconds();
+#endif
 
     LEDAndLightSensor::toOffFromOn();
 }
